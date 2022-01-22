9f927fb4c55c97e86f5e7db5f5608cb3b9a2a4d0
sarahemaginnis <sarahemaginnis@gmail.com>
2022-01-21 21:22:39 -0600

    started working on explorer chapter 4 and pioneer chapter 1 - it's a hodgepodge

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { getFish } from './scripts/database.js'
+import { getFish } from './database.js'
 
 const allFish = getFish()
 
@@ -14,6 +14,19 @@ for (const fish of allFish) {
     Use # for elements with an "id" attribute
  */
 const parentHTMLElement = document.querySelector("insert selector here")
-
+import { FishList } from './fishlist.js'
 
 parentHTMLElement.innerHTML = FishList()
+
+import { mostHolyFish } from './database.js'
+
+console.log(mostHolyFish())
+
+import { soldierFish } from './database.js'
+
+console.log(soldierFish())
+
+import { nonHolyFish } from './database.js'
+import { FishList } from './fishlist.js'
+
+console.log(nonHolyFish())
